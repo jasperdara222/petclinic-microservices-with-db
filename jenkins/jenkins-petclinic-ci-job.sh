6ca5cd11748f0539e567f3aa88fc8a1842a33ac1
jasperdara222 <jasperdara222@gmail.com>
2022-10-05 04:58:44 +0000

    changed a few lines on ci-jobs.sh

--- jenkins/jenkins-petclinic-ci-job.sh
+++ jenkins/jenkins-petclinic-ci-job.sh
@@ -1,2 +1,2 @@
-echo 'Running Unit Tests on petclinic Application for jasper'
+echo 'Running Unit Tests on petclinic Application for jasper Egbuniwe'
 docker run --rm -v $HOME/.m2:/root/.m2 -v `pwd`:/app -w /app maven:3.8-openjdk-11 mvn clean test
